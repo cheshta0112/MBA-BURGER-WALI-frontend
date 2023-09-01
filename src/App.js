31c44f1fac98b214cf24837dc8dba16ef4668aa4
Cheshta Pal <cheshtapal07@gmail.com>
2023-09-01 21:44:51 +0530

    added header

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles/app.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/home/Home";
+import Header from "./components/layout/Header";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
