d6734017e57ba799793a85b6ad8c6187a5131590
Cheshta Pal <cheshtapal07@gmail.com>
2023-09-01 23:52:00 +0530

    added home page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import Header from "./components/layout/Header";
 import "./styles/header.scss";
+import "./styles/home.scss";
 
 function App() {
   return (
     <Router>
-      <Header />
+      <Header isAuthenticated={true} />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
