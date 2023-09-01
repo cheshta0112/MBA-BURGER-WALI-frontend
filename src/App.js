20bc24c412b1dd980369f6882d175fdb1a157331
Cheshta Pal <cheshtapal07@gmail.com>
2023-09-01 21:47:57 +0530

    added css for header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./styles/app.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import Header from "./components/layout/Header";
+import "./styles/header.scss";
 
 function App() {
   return (
