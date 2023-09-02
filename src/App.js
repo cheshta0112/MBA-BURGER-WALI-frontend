a78ac734b10bbed5c970c28b8fcc8fd1fe3d1713
Cheshta Pal <cheshtapal07@gmail.com>
2023-09-02 18:40:09 +0530

    added cart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/home/Home";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import Contact from "./components/contact/Contact";
+import Cart from "./components/cart/Cart";
 
 import "./styles/app.scss";
 import "./styles/header.scss";
@@ -11,6 +12,7 @@ import "./styles/founder.scss";
 import "./styles/menu.scss";
 import "./styles/Footer.scss";
 import "./styles/contact.scss";
+import "./styles/cart.scss";
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
 
       <Footer />
