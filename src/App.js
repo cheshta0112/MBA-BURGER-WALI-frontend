84d6453d4169625b2e70deb842aa2292bbcadea1
Cheshta Pal <cheshtapal07@gmail.com>
2023-09-02 19:05:21 +0530

    shipping added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import Contact from "./components/contact/Contact";
 import Cart from "./components/cart/Cart";
+import Shipping from "./components/cart/Shipping";
 
 import "./styles/app.scss";
 import "./styles/header.scss";
@@ -13,6 +14,7 @@ import "./styles/menu.scss";
 import "./styles/Footer.scss";
 import "./styles/contact.scss";
 import "./styles/cart.scss";
+import "./styles/shipping.scss";
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/cart" element={<Cart />} />
+
+        <Route path="/shipping" element={<Shipping />} />
       </Routes>
 
       <Footer />
