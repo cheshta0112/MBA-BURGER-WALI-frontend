89c68df82ad67d07278df40ca04ed70b0e3c00b4
Cheshta Pal <cheshtapal07@gmail.com>
2023-09-02 13:06:39 +0530

    added contact page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import "./styles/app.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
-import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import Contact from "./components/contact/Contact";
+
+import "./styles/app.scss";
 import "./styles/header.scss";
 import "./styles/home.scss";
 import "./styles/founder.scss";
 import "./styles/menu.scss";
 import "./styles/Footer.scss";
+import "./styles/contact.scss";
 
 function App() {
   return (
@@ -15,6 +18,7 @@ function App() {
       <Header isAuthenticated={true} />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
 
       <Footer />
